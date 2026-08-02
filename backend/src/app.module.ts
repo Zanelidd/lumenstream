b1069a90e94964d7f189d87b50b347d9b00ccca5
Damien Arnaud <damien.arnaudpro@gmail.com>
2026-08-02 19:23:52 +0200

    Day 2: laser power simulator (heating + drift + noise + anomaly), RxJS flow

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { EventsGateway } from './events/events.gateway';
 import { AppService } from './app.service';
+import { SimulatorService } from './simulator/simulator.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [EventsGateway, AppService],
+  providers: [EventsGateway, AppService, SimulatorService],
 })
 export class AppModule {}
